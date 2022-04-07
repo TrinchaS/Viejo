2332f0aedef15c48b5ca28125a043b95a38169fb
Cesar Rodrigo Rullan <cesarodrullan@gmail.com>
2022-04-07 19:07:20 -0300

    Implementando bootstrap

--- script.js
+++ script.js
@@ -1,23 +1,110 @@
-function edit_acerca(texto,entrada){
-    document.getElementById(String(entrada)).style.display="block";
-
-    let text = document.getElementById(texto).innerText;
-    console.log(text);
+function login(){
+    //completar
 }
 
-function f(valor){
-    document.getElementById("texto-acerca").innerText=valor;    
+function escrituraContinua(valor,lugar){
+    document.getElementById(lugar).innerText=valor;
 }
 
-let textarea=document.getElementById("input-acerca");
-textarea.addEventListener('keyup',(e)=>{
-    logMessage(`Key "${e.Key}" released [event: Keyup]`);
+//AGREGAMOS EVENTOS
+//presentacion
+document.getElementById("inputPerfil").addEventListener('keyup',(e)=>{
     if(e.key=="Enter"){
-        document.getElementById("input-acerca").style.display="none"
-        
+        document.getElementById("inputPerfil").style.display="none";
+    }
+});
+//acerca
+document.getElementById("inputAcerca").addEventListener('keyup',(e)=>{
+    if(e.key=="Enter"){
+        document.getElementById("inputAcerca").style.display="none"
     }
 });
 
-function logMessage(msj){
-    console.log(msj + "<br>");
+//input Experiencia
+document.getElementById("inputExperiencia").addEventListener('keyup',(e)=>{
+    if(e.key=="Enter"){
+        document.getElementById("inputExperiencia").style.display="none"
+    }
+});
+
+//input Educacion
+document.getElementById("inputEducacion").addEventListener('keyup',(e)=>{
+    if(e.key=="Enter"){
+        document.getElementById("inputEducacion").style.display="none"
+    }
+});
+
+//input Skill
+document.getElementById("inputHabilidades").addEventListener('keyup',(e)=>{
+    if(e.key=="Enter"){
+        document.getElementById("inputHabilidades").style.display="none"
+    }
+});
+//input Proyecto
+document.getElementById("inputProyecto").addEventListener('keyup',(e)=>{
+    if(e.key=="Enter"){
+        document.getElementById("inputProyecto").style.display="none"
+    }
+});
+
+//Funciones menubar
+function login(){
+    console.log("login");
+}
+
+//FUNCIONES sobre cartas
+function editPerfil(){
+    document.getElementById("inputPerfil").style.display="block";
+}
+
+function editAcerca(){
+    document.getElementById("inputAcerca").style.display="block";
+}
+
+function agregaExperiencia(){
+    document.getElementById("inputExperiencia").style.display="block";
+}
+
+function editExperiencia(){
+    console.log("editExperiencia");
+}
+
+function borraExperiencia(){
+    console.log("borraExperiencia");
+}
+
+function agregaEducacion(){
+    document.getElementById("inputEducacion").style.display="block";
+}
+
+function editEducacion(){
+    console.log("editEducacion");
+}
+
+function borraEducacion(){
+    console.log("borraEducacion");
+}
+
+function agregaSkill(){
+    document.getElementById("inputHabilidades").style.display="block";
+}
+
+function editHabilidad(){
+    console.log("edita Habilidad");
+}
+
+function borraHabilidad(){
+    console.log("borra habilidad");
+}
+
+function agregaProyecto(){
+    document.getElementById("inputProyecto").style.display="block";
+}
+
+function editProyecto(){
+    console.log("editProyecto");
+}
+
+function borraProyecto(){
+    console.log("borraProyecto");
 }
